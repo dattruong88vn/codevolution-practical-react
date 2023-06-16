5835176cfa879b0d4f60137126cbcad9d906a7dc
dattruong88vn <dattt1.ho@mcredit.com.vn>
2023-06-16 17:21:20 +0700

    modal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import Modal from 'react-modal'
 import './App.css';
+import { useState } from 'react';
+
+Modal.setAppElement('#root')
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(true)
+  }
+
+  const closeModal = () => {
+    setShowModal(false)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={openModal}>Show modal</button>
+      <Modal isOpen={showModal} onRequestClose={closeModal} shouldCloseOnOverlayClick={false} style={{
+        overlay: {
+          backgroundColor: 'gray'
+        },
+        content: {
+          color: 'orange'
+        }
+      }}>
+        <h2>Modal title</h2>
+        <p>Modal content</p>
+        <button onClick={closeModal}>Close</button>
+      </Modal>
     </div>
   );
 }
