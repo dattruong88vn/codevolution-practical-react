6bf15b784a35a1ac74314d3a5a0a589cab208864
dattruong88vn <dattt1.ho@mcredit.com.vn>
2023-06-16 17:37:54 +0700

    tooltip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
+
 import './App.css';
+import { forwardRef } from 'react';
+
+const ColorTooltip = () => {
+  return <span style={{ color: 'yellow' }}>Color Component</span>
+}
+
+const ColorComponent = forwardRef((props, ref) => {
+  return (
+    <div ref={ref}>
+      <div>Line 1</div>
+      <div>Line 2</div>
+    </div>
+  )
+})
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ padding: 20 }}>
+        <Tippy arrow={false} content="Basic tooltip">
+          <button>Hover</button>
+        </Tippy>
+      </div>
+      <div style={{ padding: 20 }}>
+        <Tippy delay={1000} content={<span style={{ color: 'green', background: "#121212" }}>Color</span>}>
+          <button>Hover</button>
+        </Tippy>
+      </div>
+      <div style={{ padding: 20 }}>
+        <Tippy placement='right' content={<ColorTooltip />}>
+          <button>Hover</button>
+        </Tippy>
+      </div>
+      <div style={{ padding: 20 }}>
+        <Tippy placement='top-start' content={<ColorTooltip />}>
+          <ColorComponent />
+        </Tippy>
+      </div>
     </div>
   );
 }
