47383cb4382674611fbb57f336827a8c484295f4
dattruong88vn <dattt1.ho@mcredit.com.vn>
2023-06-17 15:13:45 +0700

    react-color

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { ChromePicker } from "react-color"
 import './App.css';
 
 function App() {
+  const [showColorPicker, setShowColorPicker] = useState(false)
+  const [selectedColor, setSelectedColor] = useState('#FFF');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setShowColorPicker(prevShow => !prevShow)}>{showColorPicker ? "Hide color picker" : "Pick a color"}</button>
+      <h3>You selected color: {selectedColor}</h3>
+      {showColorPicker && <ChromePicker color={selectedColor} onChange={color => setSelectedColor(color.hex)} />}
     </div>
   );
 }
