b14bedc41c3b6db9a19e3b0dc377f30f5d9acd5b
dattruong88vn <dattt1.ho@mcredit.com.vn>
2023-06-17 14:40:39 +0700

    count up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import CountUp, { useCountUp } from "react-countup";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const countUpRef = useRef(null);
+
+  const { countUp, start, pauseResume, reset, update } = useCountUp({
+    ref: countUpRef,
+    duration: 5,
+    end: 10000,
+    startOnMount: false
+  })
+
+  return <div className="App">
+    <div>
+      <h1>{countUp}</h1>
+      <div ref={countUpRef} />
+      <button onClick={start}>Start</button>
+      <button onClick={reset}>Reset</button>
+      <button onClick={pauseResume}>Pause / Resume</button>
+      <button onClick={() => update(2000)}>Update</button>
     </div>
-  );
+    <h1>
+      <CountUp end={200} />
+    </h1>
+    <h1>
+      <CountUp end={200} duration={5} />
+    </h1>
+    <h1>
+      <CountUp start={500} end={1000} duration={5} />
+    </h1>
+    <h1>
+      <CountUp prefix="$" end={1000} duration={5} decimal={2} />
+    </h1>
+    <h1>
+      <CountUp suffix="USD" end={1000} duration={5} decimal={2} />
+    </h1>
+  </div>;
 }
 
 export default App;
