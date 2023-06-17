c6077980c86abe792f22652a61425b00beffbcba
dattruong88vn <dattt1.ho@mcredit.com.vn>
2023-06-17 15:35:40 +0700

    react-datepicker

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [date, setDate] = useState(new Date());
+
+  return (
+    <div className="App">
+      <DatePicker
+        selected={date}
+        onChange={(date) => setDate(date)}
+        dateFormat={"yyyy/MM/dd"}
+        // minDate={new Date()}
+        // maxDate={new Date("2023-06-30")}
+        filterDate={(date) => date.getDay() !== 6 && date.getDay() !== 0}
+        isClearable
+        showYearDropdown
+        scrollableYearDropdown
+      />
+    </div>
+  );
 }
 
 export default App;
