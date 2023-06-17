836341e2f04429403f3a9059b1e2a6856fe36e6e
dattruong88vn <dattt1.ho@mcredit.com.vn>
2023-06-17 15:25:27 +0700

    react-credit-card-2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Card from "react-credit-cards-2";
+import "react-credit-cards-2/dist/es/styles-compiled.css";
+
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+  const [expiry, setExpiry] = useState("");
+  const [cvc, setCvc] = useState("");
+  const [focus, setFocus] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card
+        name={name}
+        number={number}
+        expiry={expiry}
+        cvc={cvc}
+        focused={focus}
+      />
+      <form>
+        <input
+          type="tel"
+          name="number"
+          placeholder="Card Number"
+          onChange={(e) => setNumber(e.target.value)}
+          onFocus={(e) => setFocus(e.target.name)}
+        />
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          onChange={(e) => setName(e.target.value)}
+          onFocus={(e) => setFocus(e.target.name)}
+        />
+        <input
+          type="text"
+          name="expiry"
+          placeholder="MM/YY"
+          onChange={(e) => setExpiry(e.target.value)}
+          onFocus={(e) => setFocus(e.target.name)}
+        />
+        <input
+          type="tel"
+          name="cvc"
+          placeholder="CVC"
+          onChange={(e) => setCvc(e.target.value)}
+          onFocus={(e) => setFocus(e.target.name)}
+        />
+      </form>
     </div>
   );
 }
