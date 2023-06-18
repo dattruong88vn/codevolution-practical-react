132d3ef7fcdafa6685a5accb8e717ae7c21c34b5
dattruong88vn <dattt1.ho@mcredit.com.vn>
2023-06-18 18:32:37 +0700

    react-player

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import ReactPlayer from "react-player";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ReactPlayer
+        url="https://www.youtube.com/watch?v=HP4E33B8E7"
+        controls={true}
+        width="480"
+        height="240"
+        onReady={() => console.log("onReady callback")}
+        onPlay={() => console.log("onPlay callback")}
+        onPause={() => console.log("onPause callback")}
+        onEnded={() => console.log("onEnded callback")}
+        onError={() => console.log("EronError callback")}
+      />
+    </div>
+  );
 }
 
 export default App;
