5b5a780194685f1e84ff1b0fcdde1033716a740d
dattruong88vn <dattt1.ho@mcredit.com.vn>
2023-06-18 18:43:07 +0700

    react-spinners

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { BounceLoader, BarLoader, BeatLoader } from "react-spinners";
 import "./App.css";
 
+const loaderCSS = {
+  marginTop: "25px",
+  marginBottom: "25px",
+};
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BounceLoader loading size={24} color="red" cssOverride={loaderCSS} />
+      <BarLoader loading size={48} color="blue" cssOverride={loaderCSS} />
+      <BeatLoader loading size={72} color="green" cssOverride={loaderCSS} />
+    </div>
+  );
 }
 
 export default App;
